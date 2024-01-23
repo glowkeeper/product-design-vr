e826ac688ff5e00d38a746e7dca5cda3b0b37680
Steve Huckle <steve.huckle@gmail.com>
2024-01-23 13:44:05 +0000

    networking

--- src/ProductDesign/Assets/Scripts/NetworkTransformClient.cs
+++ src/ProductDesign/Assets/Scripts/NetworkTransformClient.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode.Components;
 
-public class NetweorkTransformClient : NetworkTransform
+public class NetworkTransformClient : NetworkTransform
 {
    protected override bool OnIsServerAuthoritative() {
     return false;
