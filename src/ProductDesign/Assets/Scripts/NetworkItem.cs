9aa143ce75b78b468e3c0243129faf325aa2c8b7
Steve Huckle <steve.huckle@gmail.com>
2024-02-27 19:34:58 +0000

    moving objects working (kind of)

--- src/ProductDesign/Assets/Scripts/NetworkItem.cs
+++ src/ProductDesign/Assets/Scripts/NetworkItem.cs
@@ -10,23 +10,21 @@ public class NetworkItem : NetworkBehaviour
 
     private bool m_HasGrabbed = false;
     private bool m_HasLetGo = false;
-    private bool m_HasMoved = false;
 
     private Vector3 m_Position;
     private Quaternion m_Rotation;
     private Vector3 m_Scale;
 
     private ulong m_ClientID;
-    private ulong m_MoveClientID;
+    private ulong m_MoveClientID = 10000000000000000;
+
+    private const double m_IdleTime = 1000; //milliseconds
+    private double m_TimeMove = 0;
 
     public override void OnNetworkSpawn()
     {
-        m_Position = transform.position;
-        m_Rotation = transform.rotation;
-        m_Scale = transform.localScale;
-
+        m_ClientID = NetworkManager.Singleton.LocalClientId;
         m_GrabInteractable = gameObject.GetComponent<XRGrabInteractable>();
-
         if (m_GrabInteractable != null)
         {
             //Debug.Log("Grabbing");
@@ -34,7 +32,7 @@ public class NetworkItem : NetworkBehaviour
             m_GrabInteractable.selectExited.AddListener(OnLetGo);
 
         } else {
-            //Debug.Log("Cannot Grab");
+            Debug.Log("Cannot Grab");
         }   
     }
 
@@ -52,25 +50,38 @@ public class NetworkItem : NetworkBehaviour
         m_HasLetGo = true;
     }  
 
+    private void Update() 
+    {
+
+    }
+
     private void FixedUpdate() 
-   {
+    {
+        var now = System.DateTime.Now.TimeOfDay.TotalMilliseconds;
         if (m_HasGrabbed) 
         { 
+            //Debug.Log("Sending info from Grabbed");
             SendInfoToServerRpc(transform.position, transform.rotation, transform.localScale, m_ClientID);            
 
         } else if ( m_HasLetGo ) {
                 
-            if( transform.hasChanged ) {
-
+            if( transform.hasChanged ) 
+            {
+                //Debug.Log("Sending info from let go");
                 SendInfoToServerRpc(transform.position, transform.rotation, transform.localScale, m_ClientID);
+                transform.hasChanged = false;
 
             } else {
                 
+                //Debug.Log("Do I ever get here?");
                 m_HasLetGo = false;
                 
             }
-        } else if (m_HasMoved ) {
-                                
+
+        } else if ( m_MoveClientID != m_ClientID && 
+                    (now - m_TimeMove) < m_IdleTime ) 
+        {
+            //Debug.Log("In here at time " + now);
             transform.position = m_Position;
             transform.rotation = m_Rotation;
             transform.localScale = m_Scale;
@@ -81,16 +92,18 @@ public class NetworkItem : NetworkBehaviour
    private void SendInfoToServerRpc(Vector3 position, Quaternion rotation, Vector3 scale, ulong clientID)
    {        
         // var networkTime = NetworkManager.Singleton.ServerTime.TimeAsFloat;
-        // Debug.Log("Sending info to clients" + position.ToString() + rotation.ToString() + scale.ToString());
+        //Debug.Log("Sending info to clients" + position.ToString() + rotation.ToString() + scale.ToString());
         SendInfoToClientRpc(position, rotation, scale, clientID);
    }
 
    [ClientRpc]
    private void SendInfoToClientRpc(Vector3 position, Quaternion rotation, Vector3 scale, ulong clientID)
-   {
-        m_Position = position; 
+   {                  
+        m_Position = position;
         m_Rotation = rotation;
         m_Scale = scale;
-        m_HasMoved = true;
+        m_MoveClientID = clientID;
+        m_TimeMove = System.DateTime.Now.TimeOfDay.TotalMilliseconds;
+        //Debug.Log("Me: " + m_ClientID + ", Mover: " + m_MoveClientID);
    }
 }
