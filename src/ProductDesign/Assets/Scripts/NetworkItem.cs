6f8387bb8485737fe1e25ef7d25e57ecd22fe7a0
Steve Huckle <steve.huckle@gmail.com>
2024-05-26 13:44:38 +0100

    working network client transform

--- src/ProductDesign/Assets/Scripts/NetworkItem.cs
+++ src/ProductDesign/Assets/Scripts/NetworkItem.cs
@@ -9,29 +9,32 @@ public class NetworkItem : NetworkBehaviour
     private XRGrabInteractable m_GrabInteractable;
 
     private Rigidbody m_RigidBody;
+    private NetworkObject m_NetObject;
 
     private Vector3 m_Position;
     private Quaternion m_Rotation;
     private Vector3 m_Scale;
+    private Vector3 m_Velocity;
+
+    private bool m_NeedsMoving = false;
 
     private ulong m_ClientID;
-    private ulong m_MoveClientID;
 
-    private const double m_IdleTime = 1000; //milliseconds
-    private double m_TimeMove = 0;
+    //private const double m_IdleTime = 1000; //milliseconds
+    //private double m_TimeMove = 0;
 
     public override void OnNetworkSpawn()
     {
-        m_ClientID = NetworkManager.Singleton.LocalClientId;
-        m_MoveClientID = m_ClientID;
-        
-        m_RigidBody = gameObject.GetComponent<Rigidbody>();
+        m_ClientID = NetworkManager.Singleton.LocalClientId;  
+        m_NetObject = gameObject.GetComponent<NetworkObject>();        
+        m_RigidBody = gameObject.GetComponent<Rigidbody>();        
         m_GrabInteractable = gameObject.GetComponent<XRGrabInteractable>();
+
         if (m_GrabInteractable != null)
         {
             //Debug.Log("Grabbing");
             m_GrabInteractable.selectEntered.AddListener(OnGrabbed);            
-            m_GrabInteractable.selectExited.AddListener(OnLetGo);
+            //m_GrabInteractable.selectExited.AddListener(OnLetGo);
 
         } else {
             Debug.Log("Cannot Grab");
@@ -40,67 +43,82 @@ public class NetworkItem : NetworkBehaviour
 
     private void OnGrabbed(SelectEnterEventArgs args)
     {
-        //Debug.Log("Sending info from OnGrabbed");
-        SendMoveToServerRpc(transform.position, transform.rotation, transform.localScale, m_ClientID);  
-        transform.hasChanged = true; 
+        //SendMoveToServerRpc(transform.position, transform.rotation, transform.localScale, m_RigidBody.velocity, m_ClientID); 
+        RequestOwnershipServerRpc(m_ClientID);
     }
 
-    private void OnLetGo(SelectExitEventArgs args)
+    // private void OnLetGo(SelectExitEventArgs args)
+    // {
+    //     //Debug.Log("Sending info from OnLetGo");
+    //     //SendMoveToServerRpc(transform.position, transform.rotation, transform.localScale, m_RigidBody.velocity, m_ClientID);
+
+    // }  
+
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     //Debug.Log("Sending info from OnCollisionEnter");
+    //     // SendMoveToServerRpc(transform.position, transform.rotation, transform.localScale, m_RigidBody.velocity, m_ClientID);
+    //     if( !IsOwner ) 
+    //     {
+    //         //Debug.Log("Sending move info from server");
+    //         //SendMoveToServerRpc(transform.position, transform.rotation, transform.localScale, m_RigidBody.velocity, m_ClientID);
+    //         RequestOwnershipServerRpc(m_ClientID);
+    //         transform.hasChanged = false; 
+    //     }  
+
+    // }
+
+    // private void GenerateMove()
+    // {        
+    //     if( IsOwner && transform.hasChanged ) 
+    //     {
+    //         //Debug.Log("Sending move info from server");
+    //         //SendMoveToServerRpc(transform.position, transform.rotation, transform.localScale, m_RigidBody.velocity, m_ClientID);
+    //         RequestOwnershipServerRpc(m_ClientID);
+    //         transform.hasChanged = false; 
+    //     }        
+    // }
+    
+    // private void ClientMove() 
+    // {
+    //     if ( !IsOwner && m_NeedsMoving) 
+    //     { 
+    //         //m_RigidBody.velocity = m_Velocity;
+    //         //Debug.Log("Client move on client" + m_Position.ToString() + m_Rotation.ToString());
+    //         m_RigidBody.Move(m_Position, m_Rotation);
+    //         m_NeedsMoving = false;
+    //         // transform.localScale = m_Scale;
+    //     }
+    // }
+
+    // private void FixedUpdate() 
+    // {
+    //     GenerateMove();  
+    //     ClientMove();
+    // }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void RequestOwnershipServerRpc(ulong clientID)
     {
-        //Debug.Log("Sending info from OnLetGo");
-        SendMoveToServerRpc(transform.position, transform.rotation, transform.localScale, m_ClientID); 
-        transform.hasChanged = true; 
-    }  
-
-    void OnCollisionEnter(Collision collision)
-    {
-        //Debug.Log("Sending info from OnCollisionEnter");
-        SendMoveToServerRpc(transform.position, transform.rotation, transform.localScale, m_ClientID);
-        transform.hasChanged = true; 
+        m_NetObject.ChangeOwnership(clientID); 
     }
 
-    private void DoMove() 
-    {
-        if ( m_MoveClientID != m_ClientID && 
-                    (System.DateTime.Now.TimeOfDay.TotalMilliseconds - m_TimeMove) < m_IdleTime ) 
-        { 
-            m_RigidBody.Move(m_Position, m_Rotation);
-            // transform.localScale = m_Scale;
-        }
-    }
+    // [ServerRpc(RequireOwnership = false)]
+    // private void SendMoveToServerRpc(Vector3 position, Quaternion rotation, Vector3 scale, Vector3 velocity, ulong clientID)
+    // {        
+    //         // var networkTime = NetworkManager.Singleton.ServerTime.TimeAsFloat;
+    //         Debug.Log("Client move from server" + position.ToString() + rotation.ToString() + scale.ToString());        
+    //         m_NetObject.ChangeOwnership(clientID);        
+    //         SendMoveToClientRpc(position, rotation, scale, velocity);
+    // }
 
-    private void GenerateMove()
-    {        
-        if( m_MoveClientID == m_ClientID && transform.hasChanged ) 
-        {
-            //Debug.Log("Sending info from let go");
-            SendMoveToServerRpc(transform.position, transform.rotation, transform.localScale, m_ClientID);
-            transform.hasChanged = false; 
-        }        
-    }
-
-    private void FixedUpdate() 
-    {
-        GenerateMove();
-    }
-
-   [ServerRpc(RequireOwnership = false)]
-   private void SendMoveToServerRpc(Vector3 position, Quaternion rotation, Vector3 scale, ulong clientID)
-   {        
-        // var networkTime = NetworkManager.Singleton.ServerTime.TimeAsFloat;
-        //Debug.Log("Sending info to clients" + position.ToString() + rotation.ToString() + scale.ToString());
-        SendMoveToClientRpc(position, rotation, scale, clientID);
-   }
-
-   [ClientRpc]
-   private void SendMoveToClientRpc(Vector3 position, Quaternion rotation, Vector3 scale, ulong clientID)
-   {                  
-        m_Position = position;
-        m_Rotation = rotation;
-        m_Scale = scale;
-        m_MoveClientID = clientID;
-        m_TimeMove = System.DateTime.Now.TimeOfDay.TotalMilliseconds;        
-        DoMove();
-        //Debug.Log("Me: " + m_ClientID + ", Mover: " + m_MoveClientID);
-   }
+    // [ClientRpc]
+    // private void SendMoveToClientRpc(Vector3 position, Quaternion rotation, Vector3 scale, Vector3 velocity)
+    // {                  
+    //         m_Position = position;
+    //         m_Rotation = rotation;
+    //         m_Scale = scale;
+    //         m_Velocity = velocity;
+    //         m_NeedsMoving = true;   
+    // }
 }
