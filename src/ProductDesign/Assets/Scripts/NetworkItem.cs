57ac2952d3c78d823a35ca27e73bfa04973a681e
Steve Huckle <steve.huckle@gmail.com>
2024-05-29 13:28:23 +0100

    playing with rig potion

--- src/ProductDesign/Assets/Scripts/NetworkItem.cs
+++ src/ProductDesign/Assets/Scripts/NetworkItem.cs
@@ -37,78 +37,9 @@ public class NetworkItem : NetworkBehaviour
         RequestOwnershipServerRpc(m_ClientID);
     }
 
-    // private void OnLetGo(SelectExitEventArgs args)
-    // {
-    //     //Debug.Log("Sending info from OnLetGo");
-    //     //SendMoveToServerRpc(transform.position, transform.rotation, transform.localScale, m_RigidBody.velocity, m_ClientID);
-
-    // }  
-
-    // void OnCollisionEnter(Collision collision)
-    // {
-    //     //Debug.Log("Sending info from OnCollisionEnter");
-    //     // SendMoveToServerRpc(transform.position, transform.rotation, transform.localScale, m_RigidBody.velocity, m_ClientID);
-    //     if( !IsOwner ) 
-    //     {
-    //         //Debug.Log("Sending move info from server");
-    //         //SendMoveToServerRpc(transform.position, transform.rotation, transform.localScale, m_RigidBody.velocity, m_ClientID);
-    //         RequestOwnershipServerRpc(m_ClientID);
-    //         transform.hasChanged = false; 
-    //     }  
-
-    // }
-
-    // private void GenerateMove()
-    // {        
-    //     if( IsOwner && transform.hasChanged ) 
-    //     {
-    //         //Debug.Log("Sending move info from server");
-    //         //SendMoveToServerRpc(transform.position, transform.rotation, transform.localScale, m_RigidBody.velocity, m_ClientID);
-    //         RequestOwnershipServerRpc(m_ClientID);
-    //         transform.hasChanged = false; 
-    //     }        
-    // }
-    
-    // private void ClientMove() 
-    // {
-    //     if ( !IsOwner && m_NeedsMoving) 
-    //     { 
-    //         //m_RigidBody.velocity = m_Velocity;
-    //         //Debug.Log("Client move on client" + m_Position.ToString() + m_Rotation.ToString());
-    //         m_RigidBody.Move(m_Position, m_Rotation);
-    //         m_NeedsMoving = false;
-    //         // transform.localScale = m_Scale;
-    //     }
-    // }
-
-    // private void FixedUpdate() 
-    // {
-    //     GenerateMove();  
-    //     ClientMove();
-    // }
-
     [ServerRpc(RequireOwnership = false)]
     private void RequestOwnershipServerRpc(ulong clientID)
     {
         m_NetObject.ChangeOwnership(clientID); 
     }
-
-    // [ServerRpc(RequireOwnership = false)]
-    // private void SendMoveToServerRpc(Vector3 position, Quaternion rotation, Vector3 scale, Vector3 velocity, ulong clientID)
-    // {        
-    //         // var networkTime = NetworkManager.Singleton.ServerTime.TimeAsFloat;
-    //         Debug.Log("Client move from server" + position.ToString() + rotation.ToString() + scale.ToString());        
-    //         m_NetObject.ChangeOwnership(clientID);        
-    //         SendMoveToClientRpc(position, rotation, scale, velocity);
-    // }
-
-    // [ClientRpc]
-    // private void SendMoveToClientRpc(Vector3 position, Quaternion rotation, Vector3 scale, Vector3 velocity)
-    // {                  
-    //         m_Position = position;
-    //         m_Rotation = rotation;
-    //         m_Scale = scale;
-    //         m_Velocity = velocity;
-    //         m_NeedsMoving = true;   
-    // }
 }
